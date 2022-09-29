3f6edee690c2d4f14ba3cf07b1defe5e3adb2558
LeoGee1 <ucheleonard2000@gmail.com>
2022-09-29 14:42:38 +0100

    beginning the js sections

---
+++ converter.js
@@ -0,0 +1,9 @@
+const dropList = document.querySelectorAll('.select')
+
+for (let i = 0; i < dropList.length; i++) {
+  for (currencyCode in currencyList) {
+    currencyCode = currencyCode.toUpperCase()
+    let optionTag 
+  }
+    
+}
